e79deb4c0e1d312d6c246ef621b099291b0b3edb
Prabhpreet Singh <prabhpreet694@gmail.com>
2024-08-16 18:12:08 +0530

    day 19

---
+++ gph29.cpp
@@ -0,0 +1,29 @@
+// DIJKSTRA ALGORITHM (GFG)
+
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        
+        vector<int>dist(V,1e9);
+        
+        dist[S]=0;
+        pq.push({0,S});
+        
+        while(!pq.empty())
+        {
+            int currdist=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            
+            for(auto it:adj[node]){
+                int nextNode=it[0];
+                int nextDist=it[1];
+                if(nextDist+currdist < dist[nextNode]){
+                    dist[nextNode]=nextDist+currdist;
+                    pq.push({dist[nextNode],nextNode});
+                }
+            }
+            
+        }
+        return dist;
+    }
